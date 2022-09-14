52c4cda0760bfbf4264b26b7cec6e2a6f847bbce
Giuliana Cabral <cabral.giu@hotmail.com>
2022-09-13 23:27:39 -0300

    Add cards 13set 23h27

---
+++ src/components/Communities/index.js
@@ -0,0 +1,23 @@
+export function Communities () {
+    return(
+        <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap"/>
+            <div class="card-body">
+            <h5 class="card-title">Card title 1</h5>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap"/>
+            <div class="card-body">
+            <h5 class="card-title">Card title 2</h5>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap"/>
+            <div class="card-body">
+            <h5 class="card-title">Card title 3</h5>
+            </div>
+        </div>
+        </div>
+    )};
